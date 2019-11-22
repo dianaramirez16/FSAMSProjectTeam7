a1311c74091bcde5f98b5c0fa624d7f20657ca23
dianaramirez16 <dianaramirez16@aol.com>
2019-11-22 15:59:42 -0800

    added real-time clock object

--- JS/scripts.js
+++ JS/scripts.js
@@ -600,10 +600,22 @@ function run911Modal(){
   overlay2.appendTo(document.body).remove();
   return false;*/
 }
-/*
-$('.close2').click(function(){
-  $('.popup911').hide();
-  overlay2.appendTo(document.body).remove();
-  return false;
-  });
-});*/
+
+
+
+var tday=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var tmonth=["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+function GetClock(){
+var d=new Date();
+var nday=d.getDay(),nmonth=d.getMonth(),ndate=d.getDate(),nyear=d.getFullYear();
+var nhour=d.getHours(),nmin=d.getMinutes(),nsec=d.getSeconds();
+if(nmin<=9) nmin="0"+nmin;
+if(nsec<=9) nsec="0"+nsec;
+
+var clocktext=""+tday[nday]+", "+tmonth[nmonth]+" "+ndate+", "+nyear+" "+nhour+":"+nmin+":"+nsec+"";
+document.getElementById('clockbox').innerHTML=clocktext;
+}
+
+GetClock();
+setInterval(GetClock,1000);
