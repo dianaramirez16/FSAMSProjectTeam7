8d9b81d9afcd212667652966997243fa45b7867c
blueberrykary <karen.s126352@gmail.com>
2019-11-21 17:47:03 -0800

    pushing changes
    does not work yet when I submit the password and username

--- login1.php
+++ login1.php
@@ -4,14 +4,13 @@
 ?>
 
 <?
-   // error_reporting(E_ALL);
-   // ini_set("display_errors", 1);
+
 ?>
 
 <html lang = "en">
 
    <head>
-      <title>Tutorialspoint.com</title>
+      <title>Admin Login</title>
       <link href = "css/bootstrap.min.css" rel = "stylesheet">
 
       <style>
@@ -81,16 +80,18 @@
          <?php
             $msg = '';
 
-            if (isset($_POST['login']) && !empty($_POST['username'])
+            if (isset($_POST['login1']) && !empty($_POST['username'])
                && !empty($_POST['password'])) {
 
-               if ($_POST['username'] == 'tutorialspoint' &&
+               if ($_POST['username'] == 'admin' &&
                   $_POST['password'] == '1234') {
                   $_SESSION['valid'] = true;
                   $_SESSION['timeout'] = time();
-                  $_SESSION['username'] = 'tutorialspoint';
+                  $_SESSION['username'] = 'admin';
 
                   echo 'You have entered valid use name and password';
+                  header("location: Admin.php?dashboard");
+
                }else {
                   $msg = 'Wrong username or password';
                }
@@ -105,12 +106,12 @@
             ?>" method = "post">
             <h4 class = "form-signin-heading"><?php echo $msg; ?></h4>
             <input type = "text" class = "form-control"
-               name = "username" placeholder = "username = tutorialspoint"
+               name = "username" placeholder = "username = admin"
                required autofocus></br>
             <input type = "password" class = "form-control"
                name = "password" placeholder = "password = 1234" required>
             <button class = "btn btn-lg btn-primary btn-block" type = "submit"
-               name = "login">Login</button>
+               name = "login1">Login</button>
          </form>
 
          Click here to clean <a href = "logout.php" tite = "Logout">Session.
