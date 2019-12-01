562db76faf8dbaba9fdcd61d25f9357b267f7d2e
blueberrykary <karen.s126352@gmail.com>
2019-11-30 17:41:04 -0800

    not final

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php session_start(); /* Starts the session */
 if(!isset($_SESSION['UserData']['Username'])){
-header("location:login.php");
+header("location:login1.php");
 exit;
 }
 ?>
-
-Congratulation! You have logged into password protected page. <a href="logout.php">Click here</a> to Logout.
